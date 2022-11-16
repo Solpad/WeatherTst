f837afa3883fbb791a8f51e3aec3a565a4b96bd4
Solpad <rybakovartem03@gmail.com>
2022-11-17 00:13:28 +0300

    Финальная версия

--- app/src/main/java/com/example/weathertst/screens/saved/SavedFragment.kt
+++ app/src/main/java/com/example/weathertst/screens/saved/SavedFragment.kt
@@ -3,24 +3,27 @@ package com.example.weathertst.screens.saved
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.weatherapp.adapters.SavedAdapter
 import com.example.weatherapp.models.geocoding.LocationResponseItem
 import com.example.weathertst.MainActivity
 import com.example.weathertst.R
-import com.example.weathertst.adapters.SavedAdapter
-import com.example.weathertst.adapters.SearchAdapter
 import com.example.weathertst.databinding.FragmentSavedBinding
-import com.example.weathertst.databinding.FragmentSearchBinding
-import com.example.weathertst.databinding.FragmentWeekBinding
-import com.example.weathertst.screens.search.SearchFragmentViewModel
+import com.example.weathertst.model.geocoding.LocalNames
+import com.example.weathertst.repositroty.WeatherMvvmRepo
+import com.example.weathertst.screens.week.WeekFragmentViewModel
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_saved.*
+import kotlinx.android.synthetic.main.fragment_week.*
 import java.io.FileOutputStream
 import java.io.IOException
 import java.io.ObjectOutputStream
@@ -29,24 +32,40 @@ class SavedFragment : Fragment() {
 
     private var _binding: FragmentSavedBinding? = null
     private val mBinding get() = _binding!!
-    lateinit var savedAdapter: SavedAdapter
+    lateinit var adapterList: SavedAdapter
     private lateinit var mViewModel: SavedFragmentViewModel
+    private val repositoryWeather = WeatherMvvmRepo()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentSavedBinding.inflate(layoutInflater,container,false)
+        _binding = FragmentSavedBinding.inflate(layoutInflater, container, false)
+
         mViewModel = ViewModelProvider(this)[SavedFragmentViewModel::class.java]
 
-        mViewModel.printRepo()
+        mViewModel.getLocationsLive().observe(viewLifecycleOwner, Observer {
+            adapterList = SavedAdapter(it)
+            mBinding.rvSavedLocations.adapter = adapterList
+            mBinding.rvSavedLocations.layoutManager = LinearLayoutManager(context)
+            adapterList.setOnItemClickListener { location ->
+                mViewModel.selectedLocation = MutableLiveData(location)
+                repositoryWeather.setSelectedLocation(mViewModel.selectedLocation)
+
+                findNavController().navigate(R.id.action_savedFragment_to_mainFragment)
+            }
+            adapterList.setOnItemClickListenerDelete { location ->
+                mViewModel.deleteLocation(location)
+            }
+        })
+
+
         return mBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        mViewModel = ViewModelProvider(this)[SavedFragmentViewModel::class.java]
-        setupRecyclerView()
-
+        super.onViewCreated(view, savedInstanceState)
         go_search_locations_in_search_fragment.setOnClickListener {
             findNavController().navigate(R.id.action_savedFragment_to_searchFragment)
         }
@@ -55,35 +74,6 @@ class SavedFragment : Fragment() {
             findNavController().navigate(R.id.action_savedFragment_to_mainFragment)
         }
 
-        savedAdapter.setOnItemClickListener { location ->
-            mViewModel.selectedLocation = MutableLiveData(location)
-            saveToFile(activity as MainActivity, location)
-            findNavController().navigate(R.id.action_savedFragment_to_mainFragment)
-        }
-
     }
-
-    private fun setupRecyclerView() {
-        savedAdapter = SavedAdapter()
-        rv_saved_locations.apply {
-            adapter = savedAdapter
-            layoutManager = LinearLayoutManager(activity)
-        }
-    }
-
-    fun saveToFile(context: Context, location: LocationResponseItem) {
-        try {
-            val fileOutputStream: FileOutputStream =
-                context.openFileOutput("fileName", Context.MODE_PRIVATE)
-            val objectOutputStream = ObjectOutputStream(fileOutputStream)
-            objectOutputStream.writeObject(location)
-            objectOutputStream.close()
-            fileOutputStream.close()
-        } catch (e: IOException) {
-            Log.d("qwert", "file not save")
-            e.printStackTrace()
-        }
-    }
-
 
 }
